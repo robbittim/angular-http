3134295c5f4ec505263fc25ca5f6a11a27d7247a
tim <robbittim@gmail.com>
2022-05-15 14:39:10 -0500

    add httpInterceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,27 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
 import { AuthInterceptorService } from "./auth-interceptor.service";
+import { LoggingInterceptorService } from "./logging-interceptor.service";
 
 @NgModule({
 	declarations: [AppComponent],
 	imports: [BrowserModule, FormsModule, HttpClientModule],
 	providers: [
+		/*
+		the order of the below HTTP_INTERCEPTORS matters: 
+			- The run order as: 
+				1. Auth
+				2. Logging
+		*/
+
 		{
 			provide: HTTP_INTERCEPTORS,
 			useClass: AuthInterceptorService,
+			multi: true,
+		},
+		{
+			provide: HTTP_INTERCEPTORS,
+			useClass: LoggingInterceptorService,
 			multi: true,
 		},
 	],
--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -9,11 +9,13 @@ interceptor:
 
 import {
 	HttpEvent,
+	HttpEventType,
 	HttpHandler,
 	HttpInterceptor,
 	HttpRequest,
 } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { reduce, tap } from "rxjs/operators";
 
 //below code will run for every requests that leaves the app, unless manually restrict
 export class AuthInterceptorService implements HttpInterceptor {
@@ -21,11 +23,14 @@ export class AuthInterceptorService implements HttpInterceptor {
 		req: HttpRequest<any>,
 		next: HttpHandler
 	): Observable<HttpEvent<any>> {
-		//restrict the interceptor for specific url
+		//ex: restrict the interceptor for specific url
 		if (req.url != "someURL") {
 		}
 
-		console.log("Request is on its way");
-		return next.handle(req);
+		const modifedRequest = req.clone({
+			headers: req.headers.append("Auth", "xyz"),
+		});
+
+		return next.handle(modifedRequest);
 	}
 }
